6336229434d85933ebccae9bc31fad8c14678886
Pramod Yellur Gururaj <ygpramod@gmail.com>
2024-04-28 18:07:15 +0800

    Link Meals and Categories screens

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 
 import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/models/category.dart';
+import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _onSelectCategory(BuildContext context, Category category) {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (ctx) => Meals(
+        title: category.title,
+        meals: dummyMeals
+            .where((it) => it.categories.contains(category.id))
+            .toList(),
+      ),
+    ));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +36,12 @@ class CategoriesScreen extends StatelessWidget {
         children: [
           // availableCategories.map((category) => CategoryGridItem(category: category)).toList()
           for (final category in availableCategories)
-            CategoryGridItem(category: category)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () => {
+                _onSelectCategory(context, category)
+              },
+            )
         ],
       ),
     );
