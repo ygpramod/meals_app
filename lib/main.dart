b9f13dc56db63ecafc289cc724d61b434904909b
Pramod Yellur Gururaj <ygpramod@gmail.com>
2024-04-30 05:18:41 +0800

    Add Tabs Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
-
-// import 'package:meals_app/data/dummy_data.dart';
-import 'package:meals_app/screens/categories.dart';
-import 'package:meals_app/screens/meals.dart';
-
-import 'data/dummy_data.dart';
-// import 'package:meals_app/screens/meals.dart';
+import 'package:meals_app/screens/tabs.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -29,7 +23,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: const CategoriesScreen()
+      home: const TabsScreen(),
     );
   }
 }
